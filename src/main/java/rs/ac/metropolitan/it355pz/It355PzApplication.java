963b0dc4712263cd03e1f5e54b2af7fe026c2c9c
Marko Stojanovic <marko.stojanovic.1983@metropolitan.ac.rs>
2025-08-31 22:20:07 +0200

    Setup project structure

---
+++ src/main/java/rs/ac/metropolitan/it355pz/It355PzApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.metropolitan.it355pz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class It355PzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(It355PzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rs/ac/metropolitan/it355pz/It355PzApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.metropolitan.it355pz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class It355PzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
